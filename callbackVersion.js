556c7369e451274fe15dd69cd0d8495a303fa18f
Amanda Allred <amanda.barnett11@yahoo.com>
2025-10-03 21:43:39 -0400

    got name to print

--- callbackVersion.js
+++ callbackVersion.js
@@ -1,47 +1,65 @@
+mowYard('Amanda', (name) => {
+    weedEat(name, (name) => {
+        trimHedges(name, (name) => {
+            collectWood(name, (name) => {
+                waterGarden(name, (name) => {
+                    doSummerChores(name, (name) => {
+
+                    })
+                })
+            })
+
+        })
+    })
+});
+
+
 function mowYard (name, callback){
-    console.log(`${name} mowed the yard`);
-    // callback stuff //
-    // time is 2000 //
+    setTimeout(() => {
+        console.log(`${name} mowed the yard`);
+        callback(name);
+    } , 2000);
 };
 
 function weedEat(name, callback){
-    // if chore is completed //
-    console.log(`${name} finished using the weed eater.`);
-    // else //
-    console.log(`${name} fell asleep after mowing the yard.`);
-    // callback stuff //
-    // time is 1500 //
-}
+    setTimeout(() => { // if chore is completed
+        console.log(`${name} finished using the weed eater.`);
+        callback(name);
+    }, 1500);
+    // else
+        // console.log(`${name} fell asleep after mowing the yard.`);
+};
 
 function trimHedges(name, callback){
-    // if chore is completed //
-    console.log(`${name} finished trimming the hedges.`);
-    // else //
-    console.log(`${name} fell asleep after weed eating the yard.`);
-    // callback stuff //
-    // time is 1000 //
+    setTimeout(() => { // if chore is completed
+        console.log(`${name} finished trimming the hedges.`);
+        callback(name);
+    }, 1000);
+    // else 
+        // console.log(`${name} fell asleep after weed eating the yard.`);
 }
 
 function collectWood(name, callback){
-    // if chore is completed //
-    console.log(`${name} finished collecting wood.`);
-    // else //
-    console.log(`${name} fell asleep after weed trimming the hedges.`);
-    // callback stuff //
-    // time is 2500 //
+    setTimeout(() => { // if chore is completed
+        console.log(`${name} finished collecting wood.`);
+        callback(name);
+    }, 2500);
+    
+    // else 
+        // console.log(`${name} fell asleep after weed trimming the hedges.`);
 }
 
 function waterGarden(name, callback){
-    // if chore is completed //
-    console.log(`${name} finished watering the garden.`);
-    // else //
-    console.log(`${name} fell asleep after collecting wood.`);
-    // callback stuff //
-    // time is 500 //
+    setTimeout(() => { // if chore is completed
+        console.log(`${name} finished watering the garden.`);
+        callback(name);
+    }, 500);
+    
+    // else 
+        // console.log(`${name} fell asleep after collecting wood.`);
 }
 
-function doSummerChores(name, callback){ //name needs to be a string literal? //
-    // if chore is completed //
+function doSummerChores(name, callback){ //name needs to be a string literal? 
+    // if chore is completed 
     console.log(`${name} finished all their chores!`);
 }
-
